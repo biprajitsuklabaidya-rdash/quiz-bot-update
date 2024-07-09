af846ce92108fdc03de0fd0fd0026edc4984aca1
biprajitsuklabaidya-rdash <biprajit.suklabaidya@rdash.io>
2024-07-09 23:59:19 +0530

    completed functions

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -32,6 +32,19 @@ def record_current_answer(answer, current_question_id, session):
     '''
     Validates and stores the answer for the current question to django session.
     '''
+    if current_question_id is None:
+        return False, "No question to answer."
+    
+    current_question = PYTHON_QUESTION_LIST[current_question_id]
+
+    if answer.lower() == current_question['answer'].lower():
+        session['score'] = session.get('score', 0) + 1
+    
+    answers = session.get('answer',[])
+    answers.append(answer)
+    session['answer'] = answers
+
+
     return True, ""
 
 
@@ -39,8 +52,14 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
+    next_question_id = current_question_id + 1 if current_question_id is not None else 0
 
-    return "dummy question", -1
+    if next_question_id < len(PYTHON_QUESTION_LIST):
+        next_question =  PYTHON_QUESTION_LIST[next_question_id]['question_text']
+        return next_question, next_question_id
+    else :
+        return None, None
+
 
 
 def generate_final_response(session):
@@ -48,5 +67,9 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
+    score = session.get('score', 0)
+    total_questions = len(PYTHON_QUESTION_LIST)
 
-    return "dummy result"
+    response = f"Quiz Completed! Your score is {score} out of {total_questions}."
+
+    return response
